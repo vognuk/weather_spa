9c0f033309afa27c427a04f63ff1e0ce698e7900
Vitalii <vognuk@gmail.com>
2021-10-17 17:33:47 +0300

    Added change of main temperature value by event on slider.

--- src/components/weatherBlock/weatherBlock.js
+++ src/components/weatherBlock/weatherBlock.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import s from "./WeatherBlock.css";
 import classNames from "classnames/bind";
 import getWeatherURL from "../../core/utils/getWeatherURL";
@@ -8,19 +8,23 @@ import forceNumber from "force-number";
 
 import sun from "../../assets/images/weather128/sun.png";
 
-let cx = classNames.bind(s);
-
 const WeatherBlock = ({ data }) => {
   getWeatherURL();
   // localStorage.setItem("city_name", data.name);
   const [temp, setTemp] = useState(data.main?.temp);
-  console.log(temp);
+
+  //multiclass switch by https://www.npmjs.com/package/classnames
+  let cx = classNames.bind(s);
   let className = cx({
     container: true,
     containerTen: data.main.temp >= 10,
     containerMinusTen: data.main.temp <= -10,
     containerPlusThirteen: data.main.temp >= 30,
   });
+
+  useEffect(() => {
+    setTemp(data.main?.temp);
+  }, [data]);
 
   const onChange = (e) => {
     const newTemp = forceNumber(e.target.value);
@@ -32,7 +36,7 @@ const WeatherBlock = ({ data }) => {
       <div className={className}>
         <h1>Weather</h1>
         <img src={sun} alt="Sun"></img>
-        <p>{String(Math.round(temp || 0))}</p>
+        <p>{String(Math.round(temp))}</p>
         <p>in {String(data?.name || "")}</p>
       </div>
       <RangeStepInput
