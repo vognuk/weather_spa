830f6d6a1cb46f679cd657a53e85bfb43eacaaa5
Vitalii <vognuk@gmail.com>
2021-10-19 14:12:05 +0300

    Build

---
+++ build/precache-manifest.d4608f6a9bc927baa81d0844287f52c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3d4a92a3af656c131e771d8c200c32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "747dbe47f0bf8f544606",
+    "url": "/static/css/2.b8803beb.chunk.css"
+  },
+  {
+    "revision": "6d53dc5b99d81c0c09fd",
+    "url": "/static/css/main.3e8cc7a7.chunk.css"
+  },
+  {
+    "revision": "747dbe47f0bf8f544606",
+    "url": "/static/js/2.8e304a10.chunk.js"
+  },
+  {
+    "revision": "7b5ca3103946744a1127ac128c22c0f5",
+    "url": "/static/js/2.8e304a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "affdd387a0910d35747b",
+    "url": "/static/js/3.18e5a5db.chunk.js"
+  },
+  {
+    "revision": "6d53dc5b99d81c0c09fd",
+    "url": "/static/js/main.66cb3568.chunk.js"
+  },
+  {
+    "revision": "a0bc32e3b31b11628305",
+    "url": "/static/js/runtime-main.752bb078.js"
+  }
+]);
