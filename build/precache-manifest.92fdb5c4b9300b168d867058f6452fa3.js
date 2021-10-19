5e7e2ee9c6eec5ab690ad3b950ae9d715c1ea598
Vitalii <vognuk@gmail.com>
2021-10-19 16:02:31 +0300

    build

---
+++ build/precache-manifest.92fdb5c4b9300b168d867058f6452fa3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fbfb3f98f370f840d2b68ea2c91bb2d",
+    "url": "/weather_spa/index.html"
+  },
+  {
+    "revision": "747dbe47f0bf8f544606",
+    "url": "/weather_spa/static/css/2.b8803beb.chunk.css"
+  },
+  {
+    "revision": "6d53dc5b99d81c0c09fd",
+    "url": "/weather_spa/static/css/main.3e8cc7a7.chunk.css"
+  },
+  {
+    "revision": "747dbe47f0bf8f544606",
+    "url": "/weather_spa/static/js/2.8e304a10.chunk.js"
+  },
+  {
+    "revision": "7b5ca3103946744a1127ac128c22c0f5",
+    "url": "/weather_spa/static/js/2.8e304a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "affdd387a0910d35747b",
+    "url": "/weather_spa/static/js/3.18e5a5db.chunk.js"
+  },
+  {
+    "revision": "6d53dc5b99d81c0c09fd",
+    "url": "/weather_spa/static/js/main.66cb3568.chunk.js"
+  },
+  {
+    "revision": "f7079a7b10cac32aa524",
+    "url": "/weather_spa/static/js/runtime-main.304d4f94.js"
+  }
+]);
